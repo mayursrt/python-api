6f27ad85b5871e421c09e9c33afdc565fc532717
Mayur Machhi <mayurmachhi19@gmail.com>
2023-01-24 00:18:09 +0530

    added .env and changed hardcoded values

--- app/db.py
+++ app/db.py
@@ -1,15 +1,14 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-
+from .config import settings
 
 while True:
     try:
-        conn = psycopg2.connect("host=localhost dbname=python-api user=postgres password=admin", cursor_factory=RealDictCursor)
+        conn = psycopg2.connect(f"host={settings.database_hostname} dbname={settings.database_name} user={settings.database_username} password={settings.database_password}", cursor_factory=RealDictCursor)
         cur = conn.cursor()
         break
     except Exception as error:
         print("Connecting to database failed")
         print("Error: ", error)
         time.sleep(2)
-
