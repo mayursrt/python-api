20218b6a20a6dddd11d29459382bee7345a8535c
Mayur Machhi <mayur.machhi@invimatic.io>
2023-01-24 17:16:22 +0530

    votes/setup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, status, HTTPException,Depends
-from .routers import posts, users, auth
+from .routers import posts, users, auth, votes
 from sqlalchemy.orm import Session
 from .db import get_db, engine
 from . import models
@@ -10,3 +10,4 @@ app = FastAPI()
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(votes.router)
